58ee777bea7b862cd9fb8e5e54336419e40ee5be
thuva03 <raganthiva@gmail.com>
2019-08-30 10:31:35 +0530

    some change

--- q1swap.java
+++ q1swap.java
@@ -9,7 +9,7 @@ class q1swap {
         x = x + y;
         y = x - y;
         x = x - y;
-        System.out.println("value of x:" + x);
+       // System.out.println("value of x:" + x);
         System.out.println("value of y:" + y);
     }
 }
